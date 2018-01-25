f390d2f0ef6f639cd55a83c1d6e887471be17308
eaboettcher <boettch3@seattleu.edu>
2018-01-24 23:49:45 -0800

    Add project files.

---
+++ GridView/Default.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GridView.Default" %>
+
+<!DOCTYPE html>
+<%--navbar is just to get it looking ok to show kenworth since this is not connected to a db the edit/update is just for show DONT ACTUALLY TRY TO COMMIT CHANGES--%>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Kenworth CSA</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">View ASN <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">UPS SCS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Discrepancy</a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>GridView</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/sandstone/bootstrap.min.css" rel="stylesheet" />
+    <link href="Content/StyleSheet.css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <br />
+        <div id="mainContainer" class="container">
+            <div class="shadowBox">
+                <div class="page-container">
+                    <div class="container">
+                        <h1 style="margin: 5px 0 20px 0;">View ASN</h1>
+                        <div class="row">
+                            <div class="col-lg-12 ">
+                                <div class="table-responsive">
+                                    <asp:GridView ID="gvPerson" runat="server" Width="100%" CssClass="table table-striped table-bordered table-hover" AutoGenerateColumns="False" OnPageIndexChanging="gvPerson_PageIndexChanging" OnRowCancelingEdit="gvPerson_RowCancelingEdit" OnRowEditing="gvPerson_RowEditing" OnRowUpdating="gvPerson_RowUpdating" OnSorting="gvPerson_Sorting" EmptyDataText="There are no data records to display.">
+                                        <Columns>
+                                            <asp:CommandField ShowEditButton="True" />
+                                            <asp:BoundField DataField="PersonID" HeaderText="PersonID" ReadOnly="True" SortExpression="PersonID" />
+                                            <asp:TemplateField HeaderText="LastName" SortExpression="LastName">
+                                                <EditItemTemplate>
+                                                    <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("LastName") %>'></asp:TextBox>
+                                                </EditItemTemplate>
+                                                <ItemTemplate>
+                                                    <asp:Label ID="Label1" runat="server" Text='<%# Bind("LastName") %>'></asp:Label>
+                                                </ItemTemplate>
+                                            </asp:TemplateField>
+                                            <asp:TemplateField HeaderText="FirstName" SortExpression="FirstName">
+                                                <EditItemTemplate>
+                                                    <asp:TextBox ID="TextBox2" runat="server" Text='<%# Bind("FirstName") %>'></asp:TextBox>
+                                                </EditItemTemplate>
+                                                <ItemTemplate>
+                                                    <asp:Label ID="Label2" runat="server" Text='<%# Bind("FirstName") %>'></asp:Label>
+                                                </ItemTemplate>
+                                            </asp:TemplateField>
+                                        </Columns>
+                                    </asp:GridView>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GridView/Default.aspx.cs
@@ -0,0 +1,201 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+#endregion Using directives
+
+namespace GridView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // The Page is accessed for the first time.
+            if (!IsPostBack)
+            {
+                // Initialize the DataTable and store it in ViewState.
+                InitializeDataSource();
+
+                // Enable the GridView paging option and specify the page size.
+                gvPerson.AllowPaging = true;
+                gvPerson.PageSize = 5;
+
+                // Enable the GridView sorting option.
+                gvPerson.AllowSorting = true;
+
+                // Initialize the sorting expression.
+                ViewState["SortExpression"] = "PersonID ASC";
+
+                // Populate the GridView.
+                BindGridView();
+            }
+        }
+
+        // Initialize the DataTable.
+        private void InitializeDataSource()
+        {
+            // Create a DataTable object named dtPerson.
+            DataTable dtPerson = new DataTable();
+
+            // Add four columns to the DataTable.
+            dtPerson.Columns.Add("PersonID");
+            dtPerson.Columns.Add("LastName");
+            dtPerson.Columns.Add("FirstName");
+
+            // Specify PersonID column as an auto increment column
+            // and set the starting value and increment.
+            dtPerson.Columns["PersonID"].AutoIncrement = true;
+            dtPerson.Columns["PersonID"].AutoIncrementSeed = 1;
+            dtPerson.Columns["PersonID"].AutoIncrementStep = 1;
+
+            // Set PersonID column as the primary key.
+            DataColumn[] dcKeys = new DataColumn[1];
+            dcKeys[0] = dtPerson.Columns["PersonID"];
+            dtPerson.PrimaryKey = dcKeys;
+
+            // Add new rows into the DataTable.
+            dtPerson.Rows.Add(null, "Davolio", "Nancy");
+            dtPerson.Rows.Add(null, "Fuller", "Andrew");
+            dtPerson.Rows.Add(null, "Leverling", "Janet");
+            dtPerson.Rows.Add(null, "Dodsworth", "Anne");
+            dtPerson.Rows.Add(null, "Buchanan", "Steven");
+            dtPerson.Rows.Add(null, "Suyama", "Michael");
+            dtPerson.Rows.Add(null, "Callahan", "Laura");
+
+            // Store the DataTable in ViewState. 
+            ViewState["dtPerson"] = dtPerson;
+        }
+
+        private void BindGridView()
+        {
+            if (ViewState["dtPerson"] != null)
+            {
+                // Get the DataTable from ViewState.
+                DataTable dtPerson = (DataTable)ViewState["dtPerson"];
+
+                // Convert the DataTable to DataView.
+                DataView dvPerson = new DataView(dtPerson);
+
+                // Set the sort column and sort order.
+                dvPerson.Sort = ViewState["SortExpression"].ToString();
+
+                // Bind the GridView control.
+                gvPerson.DataSource = dvPerson;
+                gvPerson.DataBind();
+            }
+        }
+
+        // GridView.PageIndexChanging Event
+        protected void gvPerson_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // Set the index of the new display page.  
+            gvPerson.PageIndex = e.NewPageIndex;
+
+            // Rebind the GridView control to 
+            // show data in the new page.
+            BindGridView();
+        }
+
+        // GridView.RowEditing Event
+        protected void gvPerson_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            // Make the GridView control into edit mode 
+            // for the selected row. 
+            gvPerson.EditIndex = e.NewEditIndex;
+
+            // Rebind the GridView control to show data in edit mode.
+            BindGridView();
+        }
+
+        // GridView.RowCancelingEdit Event
+        protected void gvPerson_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            // Exit edit mode.
+            gvPerson.EditIndex = -1;
+
+            // Rebind the GridView control to show data in view mode.
+            BindGridView();
+        }
+
+        // GridView.RowUpdating Event
+        protected void gvPerson_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            if (ViewState["dtPerson"] != null)
+            {
+                // Get the DataTable from ViewState.
+                DataTable dtPerson = (DataTable)ViewState["dtPerson"];
+
+                // Get the PersonID of the selected row.
+                string strPersonID = gvPerson.Rows[e.RowIndex].Cells[2].Text;
+
+                // Find the row in DateTable.
+                DataRow drPerson = dtPerson.Rows.Find(strPersonID);
+
+                // Retrieve edited values and updating respective items.
+                drPerson["LastName"] = ((TextBox)gvPerson.Rows[e.RowIndex].FindControl("TextBox1")).Text;
+                drPerson["FirstName"] = ((TextBox)gvPerson.Rows[e.RowIndex].FindControl("TextBox2")).Text;
+
+                // Exit edit mode.
+                gvPerson.EditIndex = -1;
+
+                // Rebind the GridView control to show data after updating.
+                BindGridView();
+            }
+        }
+
+        // GridView.RowDeleting Event
+        protected void gvPerson_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            if (ViewState["dtPerson"] != null)
+            {
+                // Get the DataTable from ViewState.
+                DataTable dtPerson = (DataTable)ViewState["dtPerson"];
+
+                // Get the PersonID of the selected row.
+                string strPersonID = gvPerson.Rows[e.RowIndex].Cells[2].Text;
+
+                // Find the row in DateTable.
+                DataRow drPerson = dtPerson.Rows.Find(strPersonID);
+
+                // Remove the row from the DataTable.
+                dtPerson.Rows.Remove(drPerson);
+
+                // Rebind the GridView control to show data after deleting.
+                BindGridView();
+            }
+        }
+
+        // GridView.Sorting Event
+        protected void gvPerson_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            string[] strSortExpression = ViewState["SortExpression"].ToString().Split(' ');
+
+            // If the sorting column is the same as the previous one, 
+            // then change the sort order.
+            if (strSortExpression[0] == e.SortExpression)
+            {
+                if (strSortExpression[1] == "ASC")
+                {
+                    ViewState["SortExpression"] = e.SortExpression + " " + "DESC";
+                }
+                else
+                {
+                    ViewState["SortExpression"] = e.SortExpression + " " + "ASC";
+                }
+            }
+            // If sorting column is another column, 
+            // then specify the sort order to "Ascending".
+            else
+            {
+                ViewState["SortExpression"] = e.SortExpression + " " + "ASC";
+            }
+
+            // Rebind the GridView control to show sorted data.
+            BindGridView();
+        }
+    }
+}
